c5fb82a0526586586d20e653a2a5785336c4e8f6
Shamil Gadelshin <shamilgadelshin@gmail.com>
2023-08-01 14:16:37 +0700

    Fix errors

--- src/features/configuration.rs
+++ src/features/configuration.rs
@@ -5,20 +5,18 @@ use libp2p::{identity};
 use parking_lot::Mutex;
 use std::sync::Arc;
 use futures::future::pending;
-use subspace_networking::{StubNetworkingParametersManager, Config, Node, PieceAnnouncementRequestHandler, PieceByHashRequestHandler, VoidProviderStorage, PeerInfoProvider};
+use subspace_networking::{Config, Node, PieceAnnouncementRequestHandler, PieceByHashRequestHandler, VoidProviderStorage, PeerInfoProvider};
 
 pub async fn configure_dsn(bootstrap_address: String, protocol_prefix: &'static str) -> Node {
     let ed25519_keypair = identity::ed25519::Keypair::generate();
 
     let keypair = identity::Keypair::Ed25519(ed25519_keypair);
-    let default_config = Config::new(protocol_prefix.to_string(), keypair, VoidProviderStorage, PeerInfoProvider::Client);
+    let default_config = Config::new(protocol_prefix.to_string(), keypair, VoidProviderStorage, Some(PeerInfoProvider::Client));
 
     let config_1 = Config::<VoidProviderStorage> {
         listen_on: vec!["/ip4/0.0.0.0/tcp/40001".parse().unwrap()],
         allow_non_global_addresses_in_dht: true,
-        networking_parameters_registry: StubNetworkingParametersManager
 
-        .boxed(),
         request_response_protocols: vec![
             PieceByHashRequestHandler::create(|_, _| async { None }),
             PieceAnnouncementRequestHandler::create(|_, _| async { None }),
